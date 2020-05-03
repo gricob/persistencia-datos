a24c0c8d131371c6cb7bf85c89bb0eebc60c0bdc
Gerardo Rico Botella <grico@wearemarketing.com>
2020-05-03 10:37:58 +0200

    Initial Commit

---
+++ PersistenciaDatos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersistenciaDatos
+//
+//  Created by Gerardo Rico Botella on 03/05/2020.
+//  Copyright © 2020 Gerardo Rico Botella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
